258c6d5d279f765a2ef151c929af389ef7a9ff5b
Ripeplantain <irvingmanny@gmail.com>
2023-01-14 23:34:17 +0000

    pycoded task 5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -41,7 +41,7 @@ def iamanumber(n):
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
     """display template if n is an integer"""
-    return render_template('5-number.html',n=n)
+    return render_template('5-number.html', n=n)
 
 
 if __name__ == '__main__':
