2147caea6760f5c2f713b9654a7e980c1da3cadc
Ripeplantain <irvingmanny@gmail.com>
2023-01-15 00:01:21 +0000

    storage type not db

--- models/state.py
+++ models/state.py
@@ -17,16 +17,21 @@ class State(BaseModel, Base):
     else:
         name = ''
 
-        @property
-        def cities(self):
-            '''returns the list of City instances with state_id
-                equals the current State.id
-                FileStorage relationship between State and City
-            '''
-            from models import storage
-            related_cities = []
-            cities = storage.all(City)
-            for city in cities.values():
-                if city.state_id == self.id:
-                    related_cities.append(city)
-            return related_cities
+    def __init__(self, *args, **kwargs):
+        """initializes state"""
+        super().__init__(*args, **kwargs)
+
+        if storage_type != 'db':
+            @property
+            def cities(self):
+                '''returns the list of City instances with state_id
+                    equals the current State.id
+                    FileStorage relationship between State and City
+                '''
+                from models import storage
+                related_cities = []
+                cities = storage.all(City)
+                for city in cities.values():
+                    if city.state_id == self.id:
+                        related_cities.append(city)
+                return related_cities
