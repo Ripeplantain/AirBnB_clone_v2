993333652ef80c883bfe2c6367f082262db78dbe
Ripeplantain <irvingmanny@gmail.com>
2023-01-15 20:45:50 +0000

    montanna

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,13 +1,16 @@
+#!/usr/bin/python3
+"""Importing Flask to run the web app"""
 from flask import Flask, render_template
 from models import storage
 from models.state import State
 
+
 app = Flask(__name__)
 
 
-@app.route('/state_list',strict_slashes=False)
-def state_list():
-    """Render state list to html page"""
+@app.route("/states_list", strict_slashes=False)
+def display_states():
+    """Render state_list html page to display States created"""
     states = storage.all()
     return render_template('7-states_list.html', states=states)
 
